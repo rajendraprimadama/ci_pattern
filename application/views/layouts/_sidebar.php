d874c7dbb14c2d5e86dac7491279c4e498583d1d
Rajendra Primadama <rajendragustiprimadama@gmail.com>
2020-07-11 07:34:52 +0700

    110720 - update dikit header page

--- application/views/layouts/_sidebar.php
+++ application/views/layouts/_sidebar.php
@@ -10,27 +10,14 @@
 				<img src="<?php echo base_url('assets/uploads/images/foto_profil/'.$userdata->photo); ?>" class="img-circle">
 			</div>
       <div class="pull-left info">
-        <p><?php echo $this->session->userdata('username')?></p>
+        <p class="text-uppercase"><?php echo $this->session->userdata('username')?></p>
         <!-- Status -->
-        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        <a class="text-uppercase" href="javascript::void()"><i class="fa fa-circle text-success "></i> Online</a>
       </div>
     </div>
 
-    <!-- search form (Optional) -->
-    <form action="#" method="get" class="sidebar-form">
-      <div class="input-group">
-        <input type="text" name="q" class="form-control" placeholder="Search...">
-        <span class="input-group-btn">
-              <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-              </button>
-            </span>
-      </div>
-    </form>
-    <!-- /.search form -->
-
     <!-- Sidebar Menu -->
     <ul class="sidebar-menu" data-widget="tree">
-      <li class="header">HEADER</li>
       <!-- Optionally, you can add icons to the links -->
       <li class="active"><a href="<?=base_url()?>"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
       <li><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
