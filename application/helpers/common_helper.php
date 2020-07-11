65226a1c7352b1e065b8fbb95d905a1a580bcac0
Rajendra Primadama <rajendragustiprimadama@gmail.com>
2020-07-11 07:14:04 +0700

    110720 - initialize commit

---
+++ application/helpers/common_helper.php
@@ -0,0 +1,44 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if(!function_exists('get_hash')){
+    function get_hash($PlainPassword){
+        $option=['cost'=>5];
+    	return password_hash($PlainPassword, PASSWORD_DEFAULT, $option);
+   }
+}
+
+if(!function_exists('hash_verified')){
+    function hash_verified($PlainPassword,$HashPassword){
+    	return password_verify($PlainPassword,$HashPassword) ? true : false;
+   }
+}
+
+function konfigurasi($title, $c_des=null)
+{
+    $CI = get_instance();
+    $CI->load->model('Konfigurasi_model');
+    $CI->load->model('Auth_model');
+    $auth = $CI->Auth_model->get_by_id('id');
+    $site = $CI->Konfigurasi_model->listing();
+    $data = array(
+        'title' => $title.' | '.$site['nama_website'],
+        'logo' => $site['logo'],
+        'favicon' => $site['favicon'],
+        'email' => $site['email'],
+        'no_telp' => $site['no_telp'],
+        'alamat' => $site['alamat'],
+        'facebook' => $site['facebook'],
+        'instagram' => $site['instagram'],
+        'keywords' => $site['keywords'],
+        'metatext' => $site['metatext'],
+        'about' => $site['about'],
+        'site' => $site,
+        'c_judul' => $title,
+        'c_des' => $c_des,
+        'userdata' => $auth,
+    );
+
+    return $data;
+}
---
+++ application/views/layouts/_header.php
@@ -0,0 +1,15 @@
+<header class="main-header">
+  <!-- Logo -->
+  <a href="<?php echo base_url(); ?>" class="logo">
+    <!-- mini logo for sidebar mini 50x50 pixels -->
+    <span class="logo-mini"><small>
+      <img src="http://ci-login.dama/assets/uploads/images/favicon.ico" width="80%">
+    </small></span>
+    <!-- logo for regular state and mobile devices -->
+    <span class="logo-lg">
+      <img src="http://ci-login.dama/assets/uploads/images/logo-light-codex.png" width="80%">
+    </span>
+  </a>
+  <!-- nav -->
+  <?php require_once('_nav.php') ;?>
+</header>
